2df1d97a4b7e7ee74770988b4c4eb841e04b2e45
shivraj98 <shivrajthombare4@gmail.com>
2024-10-01 12:32:41 +0530

    import added

--- src/main/java/com/example/LearningManagementSystem/controller/CourseController.java
+++ src/main/java/com/example/LearningManagementSystem/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.example.LearningManagementSystem.controller;
 
+import com.example.LearningManagementSystem.bean.CourseBean;
+import com.example.LearningManagementSystem.bean.CourseDetailsBean;
 import com.example.LearningManagementSystem.bean.ResponseBean;
 import com.example.LearningManagementSystem.service.CourseService;
 import com.example.LearningManagementSystem.utils.ResponseHandler;
@@ -10,6 +12,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
